52f7c9f05f4416c51df54832b2c47edcd2703d6d
ppmmjc <philippmjc@gmail.com>
2026-03-14 19:54:33 +0100

    fix port for railway

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ def unbekannt():
     return jsonify([])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
